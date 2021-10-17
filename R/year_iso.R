80045813b552f5586b37754c43cf685fe7ca1c78
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2021-10-17 20:46:41 +0200

    export year_iso and fix check problems

--- R/year_iso.R
+++ R/year_iso.R
@@ -1,8 +1,8 @@
-#'## f.year.iso: Transform Two-Digit Years to Four-Digit Years
-#'
+#'## year.iso: Transform Two-Digit Years to Four-Digit Years
+
 #' This function transforms two-digit years (YY) to four-digit years compliant with ISO-8601 (YYYY). It is based on the assumption that two-digit years above a certain boundary year belong to the previous century and years at or below that boundary belong to the next century.
-#'
-#'
+
+
 #' @param x A vector of two-digit years.
 #' @param boundary The boundary year. Defaults to 50 (= 1950).
 #' @param century.prev The previous century. Defaults to the 1900s (20th century).
@@ -16,6 +16,8 @@
 #' year_iso(91)
 #' year_iso(c(13, 45, 36, 57, 68))
 
+
+#' @export
 
 
 year_iso <- function(x,
