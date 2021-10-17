672651a6b4cf9f2bdcae46ec8d484dcb6d13eb06
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2021-10-17 23:17:20 +0200

    split pdftotext from its future version; export future pdftotext

--- R/future_pdf_to_txt.R
+++ R/future_pdf_to_txt.R
@@ -1,4 +1,4 @@
-#'## Parallelized Extraction of Text from PDF Documents
+#'## Parallelized Conversion of PDF to TXT
 
 #' Extracts text from PDF files and writes the result to disk as TXT files. Parallel implementation with the future package. Resulting TXT files have the same filename as the original document (only the extension is modified).
 #'
@@ -12,25 +12,7 @@
 
 
 
-pdf_to_txt <- function(x){
-    
-    ## Extract text layer from PDF
-    pdf.extracted <- pdftools::pdf_text(x)
-
-    ## TXT filename
-    txtname <- gsub("\\.pdf",
-                    "\\.txt",
-                    x,
-                    ignore.case = TRUE)
-    
-    ## Write TXT to Disk
-    utils::write.table(pdf.extracted,
-                       txtname,
-                       quote = FALSE,
-                       row.names = FALSE,
-                       col.names = FALSE)
-    
-}
+#' @export
 
 
 
